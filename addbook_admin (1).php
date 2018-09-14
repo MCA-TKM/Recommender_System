0be72b034e653c3f87df14e584afdb08ea04a7a1
alphonsalouis <42138150+alphonsalouis@users.noreply.github.com>
2018-09-13 21:55:11 -0700

    Add files via upload

---
+++ addbook_admin (1).php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<form action="#" method=post>
+  <center>
+
+  <table>
+    <tr><td><strong>BOOK REGISTRATION</strong></td></tr>
+	
+
+  <tr width="100" height="30"><td>Name:</td>
+ <td> <input type="text" name="name"></td></tr>
+  <br>
+  <tr width="100" height="30"><td>Author:</td>
+  <td><input type="text" name="author"></td></tr>
+  <br>
+  <tr width="100" height="30"><td>Price:</td>
+  <td><input type="text" name="price"></td></tr>
+  <br>
+  <tr width="100" height="30"><td>Edition:</td>
+  <td><input type="text" name="edition"></td></tr>
+  <br>
+  <tr width="100" height="30"><td>Publication:</td>
+  <td><input type="text" name="publication"></td></tr>
+  <br>
+  <tr width="100" height="30"><td>ISBN:</td>
+ <td> <input type="text" name="isbn"></td></tr>
+  <br>
+
+  
+  
+  <tr width="100" height="30"><td><strong>Upload image:</strong></td> 
+<td><input type="file" name="file" /></td></tr><br>
+
+ <tr width="100" height="30"><td>No of Copies</td>
+  <td><input type="number" name="number"></td></tr>
+  <br>
+   
+  <tr width="100" height="30"><td>Description:</td>
+  <td><input type="text" name="description"></td></tr>
+  <br>
+  
+
+<br><tr width="100" height="30"><td>
+  <input type="submit" value="Submit" name="submit"></td></tr>
+  </table>
+</center>
+</form> 
+
+<?php
+/*  if(isset($_POST['submit']))
+ {
+ $name=$_POST['name'];
+ $author=$_POST['author'];
+ $price=$_POST['price'];
+ $edition=$_POST['edition'];
+  $publication=$_POST['publication'];
+ $isbn=$_POST['isbn'];
+ echo "$name";
+
+ 
+ 
+ 
+
+
+$conn=mysqli_connect("localhost","root","","recommender");
+if (!$conn) {
+    die('Could not connect: ' . mysql_error());
+}
+
+
+
+
+
+$s="insert into tbl_book values(4,'$name','$author','$price','$edition','$publication','$isbn')";
+mysqli_query($conn, $s);
+echo "inserted";
+}
+ */
+?>
+
+
+
+</body>
+</html>
